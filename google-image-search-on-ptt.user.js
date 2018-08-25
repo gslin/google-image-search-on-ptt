c9d1d90d72ff0e533073ee7a0a774090a7c6a0cb
Gea-Suan Lin <gslin@gslin.org>
2018-08-25 14:57:45 +0800

    Implement.

---
+++ google-image-search-on-ptt.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Google Image Search on Ptt
+// @namespace    https://wiki.gslin.org/wiki/GoogleImageSearchOnPtt
+// @version      0.20180825.0
+// @description  Add Google image search buttons on Ptt.
+// @author       Gea-Suan Lin <darkkiller@gmail.com>
+// @match        https://www.ptt.cc/bbs/*/*.html
+// @grant        GM_openInTab
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function ev() {
+        let img_re = new RegExp('\.(gif|jpeg|jpg|png)$');
+        let timeout = 0;
+
+        document.querySelectorAll('#main-content a').forEach(function(el){
+            let href = el.getAttribute('href');
+            if (!href.match(img_re)) {
+                return;
+            }
+
+            let url = 'https://images.google.com/searchbyimage?image_url=' + encodeURIComponent(href);
+            setTimeout(function() {
+                GM_openInTab(url, true);
+            }, timeout);
+            timeout += 1500;
+        });
+    };
+
+    let btn = document.createElement('button');
+    btn.addEventListener('click', ev);
+    btn.innerHTML = 'Google Image Search';
+    btn.setAttribute('style', 'height: 1.5em; margin-bottom: 3px; vertical-align: text-bottom;');
+    document.querySelectorAll('.article-metaline')[2].appendChild(btn);
+})();
